664186c291702bcc0d4dc4118b5115366f402352
Mateusz Kalinowski <mateusz.kalinowski@icloud.com>
2018-12-02 07:28:23 +0100

    Updates

---
+++ precache-manifest.57d85746aed36c6ed03135e387d0d35b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31929eda1894bde6c622",
+    "url": "/ChartsApp/static/js/runtime~main.31929eda.js"
+  },
+  {
+    "revision": "6833f7bca67aa333f823",
+    "url": "/ChartsApp/static/js/main.6833f7bc.chunk.js"
+  },
+  {
+    "revision": "db312725cb0dcc764ac5",
+    "url": "/ChartsApp/static/js/1.db312725.chunk.js"
+  },
+  {
+    "revision": "6833f7bca67aa333f823",
+    "url": "/ChartsApp/static/css/main.61a28a37.chunk.css"
+  },
+  {
+    "revision": "8bda40bbbafa3cef27de43f9a9ce8e36",
+    "url": "/ChartsApp/index.html"
+  }
+];
